92514da9b1ec8b5046202e7cf2fe8b07219569f2
Chris Rebert <chris.rebert@hulu.com>
2013-05-31 15:02:33 -0700

    ignore hard-to-avoid shadowing

--- salt/modules/debconfmod.py
+++ salt/modules/debconfmod.py
@@ -99,7 +99,11 @@ def _set_file(path):
     __salt__['cmd.run_stdout'](cmd)
 
 
-def set_(package, question, type, value, *extra):
+def set_(package,
+         question,
+         type,  # pylint: disable=W0622
+         value,
+         *extra):
     '''
     Set answers to debconf questions for a package.
 
