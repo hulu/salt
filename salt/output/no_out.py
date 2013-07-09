b007f32ec996d5a3155cee3249c2c62c91e7dcb0
Chris Rebert <chris.rebert@hulu.com>
2013-07-09 15:29:34 -0700

    legit ignorance

--- salt/output/no_out.py
+++ salt/output/no_out.py
@@ -5,7 +5,7 @@ Display no output.
 def __virtual__():
     return 'quiet'
 
-def output(ret):
+def output(ret):  # pylint: disable=W0613
     '''
     Don't display data. Used when you only are interested in the
     return.
